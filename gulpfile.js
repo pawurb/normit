15b930ba178c49d55e77e9dcdcc2edb74fc84d62
pawurb <p.urbanek89@gmail.com>
2014-07-14 19:11:48 +0200

    configured gulp jshint and watching and tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-var gulp  = require('gulp');
-var watch = require('gulp-watch');
+var gulp   = require('gulp');
+var watch  = require('gulp-watch');
 var jshint = require('gulp-jshint');
-var mocha = require('gulp-mocha');
+var mocha  = require('gulp-mocha');
 
 gulp.task('default', function() {
-  gulp.watch(['lib/*.js'], ['lint']);
+  gulp.watch(['lib/*.js'], ['lint'])
 });
 
 gulp.task('lint', function() {
   return gulp.src('./lib/*.js')
-  .pipe(jshint());
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
 });
 
 gulp.task('test', function () {
-  return gulp.src('./test/test.js', {read: false})
+  return gulp.src('./test/normit_test.js', {read: false})
   .pipe(mocha({reporter: 'nyan'}));
 });
