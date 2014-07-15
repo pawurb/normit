99dbcf98c892861d5c972edb464388825a079747
pawurb <p.urbanek89@gmail.com>
2014-07-16 00:56:27 +0200

    use promise for fetching http data

--- test/dataFetcher_test.js
+++ test/dataFetcher_test.js
@@ -3,18 +3,15 @@ var nock = require('nock');
 
 describe('dataFetcher', function() {
   var fetcher = require("./../lib/dataFetcher");
-  var response;
   before(function() {
     nock('http://target-url.com').post('/data')
     .reply(200, 'correct data');
-    var callback = function(err, res, body) {
-      response = body;
-    };
-
-    fetcher.fetch('http://target-url.com/data', 'text', callback);
   });
 
   it('fetches the correct data', function(){
-    expect(response).to.eq('correct data');
+    fetcher.fetch('http://target-url.com/data', 'text').then(function(err, res, body) {
+        expect(body).to.eq('correct data');
+      }
+    );
   });
 });
