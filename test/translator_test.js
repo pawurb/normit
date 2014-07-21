1122e969f8cc36424c053aeed328eba5dbbf5d37
pawurb <p.urbanek89@gmail.com>
2014-07-22 00:06:25 +0200

    translator returns a promise now

--- test/translator_test.js
+++ test/translator_test.js
@@ -15,19 +15,16 @@ describe('translator', function() {
     synonyms: false
   };
 
-  before(function(done) {
+  before(function() {
     var googleResponse = fs.readFileSync('./test/fixtures/rawTextResponse', 'utf8');
     nock('http://translate.google.com').post('/translate_a/t?client=t&sl=en&tl=es&hl=pl&sc=2&ie=UTF-8&oe=UTF-8&prev=enter&ssel=0&tsel=0&')
     .reply(200, googleResponse);
     sinon.spy(console, 'log');
-    var callback = function() {
-      done();
-    };
-
-    tr.run(options, callback);
   });
 
   it('works', function(){
-    expect(console.log).to.have.been.calledWith('=> burro');
+    tr.run(options).then(function() {
+      expect(console.log).to.have.been.calledWith('=> burro');
+    });
   });
 });
